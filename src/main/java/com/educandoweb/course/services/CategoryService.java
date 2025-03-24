8a215b8db387a1fe88c718a6828bfb4a62717a98
jaqdc <jaquelinedicarlosocial@gmail.com>
2025-03-24 12:02:31 -0300

    Product

--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.course.entities.enums.Category;
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.repositories.CategoryRepository;
 
 @Service
